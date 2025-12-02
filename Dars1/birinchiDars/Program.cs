330e6a81ff599a5602f96625fad83594e2453072
UbaydullayevHadyatillo <ubaydullayevhp@gmail.com>
2025-12-02 17:43:03 +0500

    done

---
+++ Dars1/birinchiDars/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace birinchiDars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hadyatllo Ubaydullayev ");
+        }
+    }
+}
