f963e7ed1675d3b133d547dda5034844ff937eef
UbaydullayevHadyatillo <ubaydullayevhp@gmail.com>
2025-12-03 22:39:41 +0500

    homeworks1.1.3

--- Dars1/homevork1.1.2/Program.cs
+++ Dars1/homevork1.1.2/Program.cs
@@ -3,7 +3,7 @@
     internal class Program
     {
         static void Main(string[] args)
-        { ////////homeworks1.1.2//////////////////////////////////
+        { /////////////////////////////////////homeworks/1.1.2///////////////////////
           //1
           //Console.Write(" sonni kiritng : ");
           //int intValue = int.Parse(Console.ReadLine());
@@ -101,22 +101,151 @@
           //float a = (intValue1 + intValue2 + intValue3 + intValue4 + intValue5 + intValue6 + intValue7 + intValue8) / 8;
           //Console.WriteLine(a);
           //13
-          Console.Write("1-sonni kiriting :");
-            int intValue1 = int.Parse(Console.ReadLine());
-            Console.Write("2-sonni kiriting :");
-            int intValue2 = int.Parse(Console.ReadLine());
-            int a = intValue1 + intValue2;
-            int b = intValue1 - intValue2;
-            int c = intValue1 * intValue2;
-            int d = intValue1 / intValue2;
-            Console.Write(" yigindisi = ");
+          //Console.Write("1-sonni kiriting :");
+          //  int intValue1 = int.Parse(Console.ReadLine());
+          //  Console.Write("2-sonni kiriting :");
+          //  int intValue2 = int.Parse(Console.ReadLine());
+          //  int a = intValue1 + intValue2;
+          //  int b = intValue1 - intValue2;
+          //  int c = intValue1 * intValue2;
+          //  int d = intValue1 / intValue2;
+          //  Console.Write(" yigindisi = ");
+          //  Console.WriteLine(a);
+          //  Console.Write(" ayirmasi = ");
+          //  Console.WriteLine(b);
+          //  Console.Write(" kopaytmasi = ");
+          //  Console.WriteLine(c);
+          //  Console.Write(" bo'linmasi = ");
+          //  Console.WriteLine(d);
+          //////////////////////////////////////homeworks/1.1.3//////////////////////////
+          //1
+          //Console.Write(" sonni kiriting : ");
+          //int intValue = int.Parse(Console.ReadLine());
+          //int a = intValue / 10;
+          //Console.WriteLine(a);
+          //2
+          //Console.Write(" 3 xonali son kiriting : ");
+          //int intValue = int.Parse(Console.ReadLine());
+          //int a = intValue / 10 % 10;
+          //Console.Write(a);
+          //3
+          //Console.Write(" 3 xonali soni kiriting : ");
+          //int intValue = int.Parse(Console.ReadLine());
+          //int a = intValue % 100 % 10;
+          //Console.Write(a);
+          //4
+          //Console.Write(" 5 xonali sonni kiriting : ");
+          //int intValue = int.Parse(Console.ReadLine());
+          //int a = intValue % 10;
+          //Console.Write(a);
+          //6
+          //Console.Write(" 3 xonali soni kiriting : ");
+          //int intValue = int.Parse(Console.ReadLine());
+          //int r1 = intValue / 100;
+          //int r10 = intValue / 10 % 10;
+          //int r100 = intValue % 10;
+          //int a = r1 + r10 + r100;
+          //Console.WriteLine(a);
+          //7
+          //Console.Write(" 3 xonali soni kiriting : ");
+          //int intValue = int.Parse(Console.ReadLine());
+          //int r1 = intValue / 100;
+          //int r10 = intValue / 10 % 10;
+          //int r100 = intValue % 10;
+          //int a = r1 * r10 * r100;
+          //Console.WriteLine(a);
+          //8
+          //Console.Write(" 2 xonali soni kiriting : ");
+          //int intValue = int.Parse(Console.ReadLine());
+          //int r1 = intValue / 10;
+          //int r10 = intValue % 10;
+          //int a = r1 * r10;
+          //Console.WriteLine(a);
+          //9
+          //Console.Write(" 5 xonali soni kiriting : ");
+          //int intValue = int.Parse(Console.ReadLine());
+          //int r1 = intValue / 10000;
+          //int r10 = intValue / 1000 % 10;
+          //int r100 = intValue / 100 % 10;
+          //int r1000 = intValue / 10 % 10;
+          //int r10000 = intValue % 10;
+          //int a = r1 + r10 + r100 + r1000 + r10000;
+          //Console.Write(a);
+          //10
+          //Console.Write(" 3 xonali soni kiriting : ");
+          //int intValue = int.Parse(Console.ReadLine());
+          //int r1 = intValue / 100;
+          //int r10 = intValue / 10 % 10;
+          //int r100 = intValue % 10;
+          //int a = r100 * 100 + r10 * 10 + r1;
+          //Console.WriteLine(a);
+          //11
+          //Console.Write(" 4 xonali soni kiriting : ");
+          //int intValue = int.Parse(Console.ReadLine());
+          //int r1 = intValue / 1000;
+          //int r10 = intValue / 100 % 10;
+          //int r100 = intValue / 10 % 10;
+          //int r1000 = intValue% 10;
+          //int a = r10 * 1000 + r1 * 100 + r100 * 10 + r1000;
+          //Console.Write(a);
+          //12
+          //Console.Write(" 3 xonali soni kiriting : ");
+          //int intValue = int.Parse(Console.ReadLine());
+          //int r1 = intValue / 100;
+          //int r10 = intValue / 10 % 10;
+          //int r100 = intValue % 10;
+          //int a = r1 * 100 + r100 * 10 + r10;
+          //Console.WriteLine(a);
+          ////13
+          //Console.Write(" 4 xonali soni kiriting : ");
+          //int intValue = int.Parse(Console.ReadLine());
+          //int r1 = intValue / 1000;
+          //int r10 = intValue / 100 % 10;
+          //int r100 = intValue / 10 % 10;
+          //int r1000 = intValue % 10;
+          //int a = r1 * 1000 + r1000 * 100 + r100 * 10 + r10;
+          //Console.Write(a);
+          //14
+          //Console.Write(" GB ni kiriting : ");
+          //int intValue = int.Parse(Console.ReadLine());
+          //int a = intValue * 1024;
+          //Console.Write(a);
+          //15
+          //Console.Write(" GB ni kiriting : ");
+          //int intValue = int.Parse(Console.ReadLine());
+          //int a = intValue * 1024 * 1024;
+          //Console.Write(a);
+          //16
+          //Console.Write(" GB ni kiriting : ");
+          //int intValue = int.Parse(Console.ReadLine());
+          //int a = intValue / 1024;
+          //Console.Write(a);
+          //17
+          //Console.Write(" GB ni kiriting : ");
+          //int intValue = int.Parse(Console.ReadLine());
+          //int a = intValue / 2;
+          //Console.Write(a);
+          //18
+          //Console.Write(" GB ni kiriting : ");
+          //int intValue = int.Parse(Console.ReadLine());
+          //int a = intValue / 5;
+          //Console.Write(a);
+          //19
+          //Console.Write(" 3 xonali soni kiriting : ");
+          //int intValue = int.Parse(Console.ReadLine());
+          //int r1 = intValue / 100;
+          //int r10 = intValue / 10 % 10;
+          //int r100 = intValue % 10;
+          //int a = r100 * 100 + r10 * 10 + r1;
+          //Console.WriteLine(a);
+          //20
+          Console.Write(" 3 xonali soni kiriting : ");
+            int intValue = int.Parse(Console.ReadLine());
+            int r1 = intValue / 100;
+            int r10 = intValue / 10 % 10;
+            int r100 = intValue % 10;
+            int a = r10 * 100 + r100 * 10 + r1;
             Console.WriteLine(a);
-            Console.Write(" ayirmasi = ");
-            Console.WriteLine(b);
-            Console.Write(" kopaytmasi = ");
-            Console.WriteLine(c);
-            Console.Write(" bo'linmasi = ");
-            Console.WriteLine(d);
         }
     }
 }
