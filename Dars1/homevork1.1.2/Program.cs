c5b964f9c52eb7a2e5b4592d09040e10850a0244
UbaydullayevHadyatillo <ubaydullayevhp@gmail.com>
2025-12-03 22:33:28 +0500

    1-vazifa

---
+++ Dars1/homevork1.1.2/Program.cs
@@ -0,0 +1,122 @@
+﻿namespace homevork1._1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { ////////homeworks1.1.2//////////////////////////////////
+          //1
+          //Console.Write(" sonni kiritng : ");
+          //int intValue = int.Parse(Console.ReadLine());
+          //int a = intValue + 10;
+          //Console.Write(a);
+          //2
+          //Console.Write(" sonni kiritng : ");
+          //int intValue = int.Parse(Console.ReadLine());
+          //int a = intValue - 10;
+          //Console.Write(a);
+          //3
+          //Console.Write(" sonni kiritng : ");
+          //int intValue = int.Parse(Console.ReadLine());
+          //int a = intValue * 10;
+          //Console.Write(a);
+          //4
+          //Console.Write(" sonni kiritng : ");
+          //int intValue = int.Parse(Console.ReadLine());
+          //int a = intValue * intValue;
+          //Console.Write(a);
+          //5
+          //Console.Write(" sonni kiritng : ");
+          //int intValue = int.Parse(Console.ReadLine());
+          //int a = intValue * intValue * intValue;
+          //Console.Write(a);
+          //6
+          //Console.Write(" sonni kiritng : ");
+          //int intValue = int.Parse(Console.ReadLine());
+          //int a = intValue * intValue * intValue * intValue * intValue;
+          //Console.Write(a);
+          //7
+          //Console.Write(" 1-sonni kiritng : ");
+          //int intValue1 = int.Parse(Console.ReadLine());
+          //Console.Write(" 2-sonni kiritng : ");
+          //int intValue2 = int.Parse(Console.ReadLine());
+          //int a = intValue1 + intValue2;
+          //Console.Write(a);
+          //8
+          //Console.Write(" 1-sonni kiritng : ");
+          //int intValue1 = int.Parse(Console.ReadLine());
+          //Console.Write(" 2-sonni kiritng : ");
+          //int intValue2 = int.Parse(Console.ReadLine());
+          //int a = intValue1 * intValue2;
+          //Console.Write(a);
+          //9
+          //Console.Write(" 1-sonni kiritng : ");
+          //int intValue1 = int.Parse(Console.ReadLine());
+          //Console.Write(" 2-sonni kiritng : ");
+          //int intValue2 = int.Parse(Console.ReadLine());
+          //Console.Write(" 3-sonni kiritng : ");
+          //int intValue3 = int.Parse(Console.ReadLine());
+          //Console.Write(" 4-sonni kiritng : ");
+          //int intValue4 = int.Parse(Console.ReadLine());
+          //int a = intValue1 + intValue2 + intValue3 + intValue4;
+          //Console.WriteLine(a);
+          //10
+          //Console.Write(" 1-sonni kiritng : ");
+          //float intValue1 = float.Parse(Console.ReadLine());
+          //Console.Write(" 2-sonni kiritng : ");
+          //float intValue2 = float.Parse(Console.ReadLine());
+          //Console.Write(" 3-sonni kiritng : ");
+          //float intValue3 = float.Parse(Console.ReadLine());
+          //float a = (intValue1 + intValue2 + intValue3) / 3;
+          //Console.WriteLine(a);
+          //11
+          //Console.Write(" 1-sonni kiritng : ");
+          //float intValue1 = float.Parse(Console.ReadLine());
+          //Console.Write(" 2-sonni kiritng : ");
+          //float intValue2 = float.Parse(Console.ReadLine());
+          //Console.Write(" 3-sonni kiritng : ");
+          //float intValue3 = float.Parse(Console.ReadLine());
+          //Console.Write(" 4-sonni kiritng : ");
+          //float intValue4 = float.Parse(Console.ReadLine());
+          //Console.Write(" 5-sonni kiritng : ");
+          //float intValue5 = float.Parse(Console.ReadLine());
+          //float a = (intValue1 + intValue2 + intValue3 + intValue4 + intValue5) / 5;
+          //Console.WriteLine(a);
+          //12
+          //Console.Write(" 1-sonni kiritng : ");
+          //float intValue1 = float.Parse(Console.ReadLine());
+          //Console.Write(" 2-sonni kiritng : ");
+          //float intValue2 = float.Parse(Console.ReadLine());
+          //Console.Write(" 3-sonni kiritng : ");
+          //float intValue3 = float.Parse(Console.ReadLine());
+          //Console.Write(" 4-sonni kiritng : ");
+          //float intValue4 = float.Parse(Console.ReadLine());
+          //Console.Write(" 5-sonni kiritng : ");
+          //float intValue5 = float.Parse(Console.ReadLine());
+          //Console.Write(" 6-sonni kiritng : ");
+          //float intValue6 = float.Parse(Console.ReadLine());
+          //Console.Write(" 7-sonni kiritng : ");
+          //float intValue7 = float.Parse(Console.ReadLine());
+          //Console.Write(" 8-sonni kiritng : ");
+          //float intValue8 = float.Parse(Console.ReadLine());
+          //float a = (intValue1 + intValue2 + intValue3 + intValue4 + intValue5 + intValue6 + intValue7 + intValue8) / 8;
+          //Console.WriteLine(a);
+          //13
+          Console.Write("1-sonni kiriting :");
+            int intValue1 = int.Parse(Console.ReadLine());
+            Console.Write("2-sonni kiriting :");
+            int intValue2 = int.Parse(Console.ReadLine());
+            int a = intValue1 + intValue2;
+            int b = intValue1 - intValue2;
+            int c = intValue1 * intValue2;
+            int d = intValue1 / intValue2;
+            Console.Write(" yigindisi = ");
+            Console.WriteLine(a);
+            Console.Write(" ayirmasi = ");
+            Console.WriteLine(b);
+            Console.Write(" kopaytmasi = ");
+            Console.WriteLine(c);
+            Console.Write(" bo'linmasi = ");
+            Console.WriteLine(d);
+        }
+    }
+}
