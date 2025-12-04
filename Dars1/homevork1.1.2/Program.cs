70958f6703f40d78abd085c290f51e1a225eaf4f
UbaydullayevHadyatillo <ubaydullayevhp@gmail.com>
2025-12-04 13:34:39 +0500

    homeworks1.1.4

--- Dars1/homevork1.1.2/Program.cs
+++ Dars1/homevork1.1.2/Program.cs
@@ -239,13 +239,149 @@
           //int a = r100 * 100 + r10 * 10 + r1;
           //Console.WriteLine(a);
           //20
-          Console.Write(" 3 xonali soni kiriting : ");
-            int intValue = int.Parse(Console.ReadLine());
-            int r1 = intValue / 100;
-            int r10 = intValue / 10 % 10;
-            int r100 = intValue % 10;
-            int a = r10 * 100 + r100 * 10 + r1;
-            Console.WriteLine(a);
+          //Console.Write(" 3 xonali soni kiriting : ");
+          //int intValue = int.Parse(Console.ReadLine());
+          //int r1 = intValue / 100;
+          //int r10 = intValue / 10 % 10;
+          //int r100 = intValue % 10;
+          //int a = r10 * 100 + r100 * 10 + r1;
+          //Console.WriteLine(a);
+          ////////////////////////////////homeworks/1.1.4//////////////////
+          //1
+          //Console.Write("sonni kiriting : ");
+          //int intValue = int.Parse(Console.ReadLine());
+          //int a = intValue * intValue;
+          //Console.Write(a * a);
+          //2
+          //Console.Write("sonni kiriting : ");
+          //int intValue = int.Parse(Console.ReadLine());
+          //int a = intValue * intValue;
+          //Console.Write(a * a * a);
+          //3
+          //Console.Write("sonni kiriting : ");
+          //int intValue = int.Parse(Console.ReadLine());
+          //int a = intValue * intValue;
+          //int c = a * a;
+          //Console.Write(c * c);
+          //4
+          //Console.Write(" kunni kiriting : ");
+          //int intValue = int.Parse(Console.ReadLine());
+          //Console.Write(" soatni kiriting : ");
+          //int intValue2 = int.Parse(Console.ReadLine());
+          //int a = intValue * 24 + intValue2;
+          //Console.WriteLine(a);
+          //5
+          //Console.Write(" haftani kiriting : ");
+          //  int intValue = int.Parse(Console.ReadLine());
+          //  Console.Write(" soatni kiriting : ");
+          //  int intValue2 = int.Parse(Console.ReadLine());
+          //  int a = intValue * 7 + intValue2 / 24;
+          //  Console.WriteLine(a);
+          //6
+          //Console.Write(" minutni kiriting : ");
+          //  int intValue = int.Parse(Console.ReadLine());
+          //  Console.Write(" soatni kiriting : ");
+          //  int intValue2 = int.Parse(Console.ReadLine());
+          //  int a = intValue * 60 + intValue2 * 60 * 60;
+          //  Console.WriteLine(a);
+          //7
+          //Console.Write(" fayil hajmini kiriting (baytda) : ");
+          //int intValue = int.Parse(Console.ReadLine());
+          //int a = intValue / 1024;
+          //Console.Write(a);
+          //8
+          //Console.Write(" intirnet tezligini kiriting(kbayt) : ");
+          //int intValue = int.Parse(Console.ReadLine());
+          //Console.Write(" fayl hajmini kiriting(GB): ");
+          //int intValue2 = int.Parse(Console.ReadLine());
+          //int a = intValue2 * 1024 * 1024 / intValue / 60;
+          //Console.Write(a);
+          //9
+          //Console.Write(" dollorni kiriting $ : ");
+          //int intValue = int.Parse(Console.ReadLine());
+          //Console.Write(" yevroni kiriting : ");
+          //int intValue2 = int.Parse(Console.ReadLine());
+          //int a = intValue2 * 12600 + intValue2 * 1450;
+          //Console.Write(a);
+          //10
+          //Console.Write("sonni kiriting : ");
+          //int intValue = int.Parse(Console.ReadLine());
+          //int a = intValue * intValue;
+          //int c = a * a;
+          //int d = c * c;
+          //Console.Write(d * a);
+          //11
+          //Console.Write(" sahifani kiriting : ");
+          //int bet = int.Parse(Console.ReadLine());
+          //Console.Write(" qatorni kiriting : ");
+          //int qator = int.Parse(Console.ReadLine());
+          //Console.Write(" belgini kiriting : ");
+          //int belgi = int.Parse(Console.ReadLine());
+          //string res = $" kitob hajmi : {bet * qator * belgi} mb";
+          //Console.Write(res);
+          //12
+          //Console.Write(" daqiqasiga nechta bosish qobilyatiga ega : ");
+          //int num = int.Parse(Console.ReadLine());
+          //string res = $"2.5 daqiqada {num / 6 * 2.5} ta soz yozadi ";
+          //Console.WriteLine(res);
+          //13
+          //Console.Write(" birinchi sonni kiriting : ");
+          //int num1 = int.Parse(Console.ReadLine());
+          //Console.Write(" ikkinchi sonni kiriting : ");
+          //int num2 = int.Parse(Console.ReadLine());
+          //num1 = num1 + num2;
+          //num2 = num1 - num2;
+          //num1 = num1 - num2;
+          //string res = $" birinchi son {num1}";
+          //string res2 = $" ikkinchi son {num2}";
+          //Console.WriteLine(res);
+          //Console.WriteLine(res2);
+          //14
+          //Console.Write(" A sonini kiriting : ");
+          //int a = int.Parse(Console.ReadLine());
+          //Console.Write(" B sonini kiriting : ");
+          //int b = int.Parse(Console.ReadLine());
+          //Console.Write(" C sonini kiriting : ");
+          //int c = int.Parse(Console.ReadLine());
+          //int d = a;
+          //a = b;
+          //b = c;
+          //c = d;
+          //Console.WriteLine(a);
+          //Console.WriteLine(b);
+          //Console.WriteLine(c);
+          //15
+          //Console.Write(" patrikda qancha pul bor : ");
+          //int sum1 = int.Parse(Console.ReadLine());
+          //Console.Write(" julyanda qancha pul bor : ");
+          //int sum2 = int.Parse(Console.ReadLine());
+          //string res = $" patrik julyanga {(sum1 -  sum2) / 2 } som pul bersa ikkisini puli tenglashadi !!"; 
+          //Console.WriteLine(res);
+          //16
+          //Console.Write(" km ni kiriting : ");
+          //int km = int.Parse(Console.ReadLine());
+          //Console.Write(" m ni kiriting : ");
+          //int m = int.Parse(Console.ReadLine());
+          //string sum = $"{km * 1000 * 10 + m * 10} dm ";
+          //Console.Write(sum);
+          //17
+            Console.Write(" sonni kiriting : ");
+            int num = int.Parse(Console.ReadLine());
+            int a = num * num;  //2
+            int b = a * num;      //3
+            int c = b * b;      //6
+            int d = c * c;      //12
+            int e = d * b;
+            Console.WriteLine(e);
+
+
+
+
+
+
+
         }
+
+
     }
 }
